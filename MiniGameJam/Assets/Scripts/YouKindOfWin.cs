722fd498c1394895b8619ff7a49da48fa0221569
Akimaya <alice@jmail.me.uk>
2023-12-22 22:08:28 +0000

    win and lose

---
+++ MiniGameJam/Assets/Scripts/YouKindOfWin.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class YouKindOfWin : MonoBehaviour
+{
+    public void RestartButton()
+    {
+
+        SceneManager.LoadScene("SampleScene");
+
+    }
+
+    public void MenuButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
