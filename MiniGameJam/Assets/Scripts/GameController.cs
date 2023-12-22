65918897eecad504f146da3906eba4d222da107a
PandaStuff1 <stuffed.panda1423@gmail.com>
2023-12-22 16:24:16 +0000

    UI jazz I guess?

--- MiniGameJam/Assets/Scripts/GameController.cs
+++ MiniGameJam/Assets/Scripts/GameController.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
@@ -18,7 +21,10 @@ public class GameController : MonoBehaviour
     [SerializeField]
     private GameObject sheep;
     [SerializeField]
-    private Vector2 spawnPos = Vector2.zero;
+    private Vector2 spawnPos = new Vector2(10, 3);
+    [SerializeField]
+    private TextMeshPro textElement;
+
 
     void Start()
     {
@@ -44,12 +50,17 @@ public class GameController : MonoBehaviour
     private void addCounter()
     {
         counter++;
+        textElement.text = counter.ToString();
         Debug.Log(counter);
     }
 
     private void spawnSheep()
     {
         totalSheep++;
+
+        Vector2 spawnPos = new Vector2(transform.position.x, Random.Range(1.0f, 5.0f));
         Instantiate(sheep, spawnPos, Quaternion.identity);
+
+        
     }
 }
